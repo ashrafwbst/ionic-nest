1110f9cda19bd65bf8167292265b8a23d2762059
ashrafwbst <er.ashrafkamal@gmail.com>
2021-02-09 16:23:47 +0530

    init commit

---
+++ public/build/static/js/53.e533cd77.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpphoto-gallery"]=this["webpackJsonpphoto-gallery"]||[]).push([[53],{139:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return i}));var e=o(12),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.h)((function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var r=i.closest("ion-content");r&&r.componentOnReady().then((function(){Object(e.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.e533cd77.chunk.js.map
